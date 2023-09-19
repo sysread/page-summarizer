fbf69d16d796eb5bc548950c02d183216543196f
Jeff Ober <jeffober@gmail.com>
2023-09-19 16:01:05 -0600

    Update to manifest v3, add markdown handling, error handling, and preserve last result while on the same page

--- background.js
+++ background.js
@@ -1,53 +1,64 @@
+// Listen for messages from the popup
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "summarize") {
-    chrome.tabs.executeScript(
-      { code: "document.body.innerText" },
-      async (resultArray) => {
-        const pageContent = resultArray[0];
-        try {
-          const summary = await summarizeContent(pageContent);
-          sendResponse({ summary });
-        } catch (error) {
-          sendResponse({ summary: "An error occurred while summarizing." });
+    let tabId = request.tabId;
+    let config = request.config;
+
+    chrome.scripting.executeScript(
+      {
+        target: { tabId },
+        func: function () {
+          return document.body.innerText;
+        },
+      },
+      (result) => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+          return;
         }
+
+        let text = result[0].result;
+
+        // Send the request to the API
+        fetch("https://api.openai.com/v1/chat/completions", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${config.apiKey}`
+          },
+          body: JSON.stringify({
+            model: config.model,
+            messages: [{role: 'user', content: 'Summarize the text for me.'}, {role: 'system', content: text}]
+          })
+        })
+        // Parse the response
+        .then(response => response.json())
+        // Send the response to the popup
+        .then(data => {
+          // Extract the summary or error
+          let result = data.error
+            ? { summary: null, error: data.error.message }
+            : { summary: data.choices[0].message.content, error: null };
+
+          // Store the response
+          chrome.storage.local.set({ "lastResult": result });
+
+          // Send the response back to the popup
+          sendResponse(result);
+        })
+        .catch(error => { console.error("Error:", error) });
       }
     );
-    return true;
   }
+  return true;
 });
 
-function getPageContent() {
-  return document.body.innerText;
-}
-
-async function summarizeContent(content) {
-  let config = await new Promise((resolve) => {
-    chrome.storage.sync.get(["apiKey", "model", "customPrompts"], (result) => {
-      resolve(result);
+// Clear stored data when the URL changes
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url) {
+    // Clear stored data
+    chrome.storage.local.remove("lastResult", () => {
+      console.log("Cleared last result due to URL change.");
     });
-  });
-
-  const apiKey = config.apiKey;
-  const model = config.model || "gpt-3.5-turbo-16k";
-  const customPrompts = config.customPrompts || [];
-
-  const messages = [{ role: "system", content: `You are a helpful assistant that summarizes texts.` }];
-  customPrompts.forEach((prompt) => {
-    messages.push({ role: "user", content: prompt });
-  });
-  messages.push({ role: "user", content: `Summarize:\n${content}` });
-
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${apiKey}`
-    },
-    body: JSON.stringify({
-      model,
-      messages,
-    }),
-  });
-  const data = await response.json();
-  return data.choices[0].message.content.trim();
-}
+  }
+});
--- popup.js
+++ popup.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   let working = false;
   const statusElement = document.getElementById('status');
-  
+
+  // Reset the status when the popup is opened
+  chrome.storage.local.get('lastResult', function(data) {
+    if (data.lastSummary) {
+      updateStatus(data.lastSummary);
+    }
+  });
+
   function updateStatus(message) {
-    statusElement.textContent = message;
+    statusElement.innerHTML = marked.marked(message);
   }
 
   function resetStatus() {
@@ -14,16 +21,26 @@ document.addEventListener('DOMContentLoaded', function () {
     if (working) {
       return;
     }
+
     working = true;
     updateStatus('Fetching summary...');
 
-    chrome.runtime.sendMessage({ action: 'summarize' }, function (response) {
-      working = false;
-      if (response && response.summary) {
-        updateStatus(`Summary: ${response.summary}`);
-      } else {
-        updateStatus('Failed to fetch summary.');
-      }
+    chrome.storage.sync.get(['apiKey', 'model'], function (config) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let tab = tabs[0];
+
+        chrome.runtime.sendMessage({ action: 'summarize', tabId: tab.id, config: config }, function(response) {
+          working = false;
+
+          if (response && response.summary) {
+            updateStatus(`Summary: ${response.summary}`);
+          } else if (response && response.error) {
+            updateStatus(`<span style="color: red; font-style: italic;">Error: ${response.error}</span>`);
+          } else {
+            updateStatus('Failed to fetch summary.');
+          }
+        });
+      });
     });
   });
 });
