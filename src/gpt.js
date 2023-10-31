77cc70056c8f824179d408ddd38f8c4a32749b5f
Jeff Ober <jeffober@gmail.com>
2023-10-31 12:28:21 -0600

    Simplify GPT message parsing a little

--- src/gpt.js
+++ src/gpt.js
@@ -7,10 +7,35 @@ const PORT_CLOSED = 'Error: Attempting to use a disconnected port object';
 const ERR_API_KEY = 'Error: API key is not set';
 
 /*------------------------------------------------------------------------------
+ * Builds up a buffer of JSON data and returns the parsed JSON object once
+ * enough data has been received to represent a complete JSON string. If the
+ * buffer is incomplete, returns null until enough data has been received to
+ * return a complete JSON object.
+ *----------------------------------------------------------------------------*/
+function JsonBuffer() {
+  let buffer = '';
+
+  return function (data) {
+    buffer += data;
+
+    let result = null;
+
+    try {
+      result = JSON.parse(buffer);
+      buffer = '';
+    } catch (error) {
+      // do nothing
+    }
+
+    return result;
+  };
+}
+
+/*------------------------------------------------------------------------------
  * Parses the response from the openai chat completions endpoint and acts as an
- * iterator over the response. Because the response is streamed, the iterator
+ * iterator over the response. Because the response is streamed, the generator
  * builds up the aggregated response, returning the complete buffer on each
- * iteration until complete.
+ * invocation until complete.
  *
  * For example:
  *   {data: "Hello", error: null}
@@ -18,113 +43,73 @@ const ERR_API_KEY = 'Error: API key is not set';
  *   {data: "Hello, how are you?", error: null}
  *
  * If an error occurs, the error is returned in the error field and the
- * iteration is stopped.
+ * generation is stopped. After that, it will always return the same structure.
  *----------------------------------------------------------------------------*/
-class GptResponseReader {
-  constructor(response) {
-    this.reader = response.body.getReader();
-    this.buffer = '';
-    this.error = null;
-    this.done = false;
-    this.lastChunk = '';
-  }
+function GptResponseReader(response) {
+  const reader = response.body.getReader();
 
-  // Required for the async iterator protocol
-  [Symbol.asyncIterator]() {
-    return this;
-  }
+  let buffer = '';
+  let error = null;
+  let done = false;
 
-  // Required for the async iterator protocol
-  async next() {
-    if (this.done) {
-      return { done: true };
+  return async function () {
+    if (done) {
+      return { data: buffer, error: error };
     }
 
-    const { value: chunk, done: readerDone } = await this.reader.read();
+    const { value: chunk, done: readerDone } = await reader.read();
 
     if (readerDone) {
-      this.finish();
-      return this.sendResult();
+      debug('FINISH');
+      done = true;
+      return { data: buffer, error: error };
     }
 
-    return this.parseChunk(chunk);
-  }
-
-  parseChunk(chunk) {
     const string = new TextDecoder().decode(chunk);
-
     debug('RECV:', string);
 
     // Some errors are returned as the initial message, but they can be
-    // multi-line, so we have to attempt to parse them here to see if
-    // they are an error. If the chunk cannot be parsed as JSON, then it
-    // is a normal message chunk.
+    // multi-line, so we have to attempt to parse them here to see if they are
+    // an error. If the chunk cannot be parsed as JSON, then it is a normal
+    // message chunk.
     try {
       const data = JSON.parse(string);
 
       if (data.error) {
-        this.setError(data.error.message);
-        return this.sendResult();
+        error = data.error.message;
+        return { data: buffer, error: error };
       }
     } catch (error) {
       // do nothing
     }
 
     const lines = string.split('\n').filter((line) => line !== '');
+    const json_buffer = JsonBuffer();
 
     debug('LINES:', lines);
 
     for (const line of lines) {
       if (line === DONE_MARKER) {
-        this.finish();
-        return this.sendResult();
+        done = true;
+        return { data: buffer, error: error };
       } else if (line.startsWith('data: ')) {
-        const json = this.lastChunk + line.substring(6);
-
-        let data = null;
-
-        try {
-          data = JSON.parse(json);
-        } catch (error) {
-          this.lastChunk = json;
-        }
+        const data = json_buffer(line.substring(6));
 
         if (data !== null) {
           if (data.error) {
-            this.setError(data.error.message);
-            return this.sendResult();
+            error = data.error.message;
+            return { data: buffer, error: error };
           }
 
           if (data.choices[0].delta.content) {
-            this.append(data.choices[0].delta.content);
+            buffer += data.choices[0].delta.content;
           }
         }
       }
     }
 
-    return this.sendResult();
-  }
-
-  append(data) {
-    this.buffer += data;
-  }
-
-  sendResult() {
-    debug('RESULT:', { data: this.buffer, error: this.error });
-    return { done: false, value: { data: this.buffer, error: this.error } };
-  }
-
-  setError(error) {
-    debug('ERROR:', error);
-    this.error = error;
-    this.finish();
-  }
-
-  finish() {
-    debug('FINISH');
-    this.done = true;
-    this.reader.releaseLock();
-  }
+    return { data: buffer, error: error };
+  };
 }
 
 async function fetchCompletions(apiKey, payload) {
@@ -175,10 +160,16 @@ export async function fetchAndStream(port, messages) {
     };
 
     const response = await fetchCompletions(config.apiKey, payload);
-    const reader = new GptResponseReader(response);
+    const reader = GptResponseReader(response);
     let lastMessage = null;
 
-    for await (const message of reader) {
+    while (true) {
+      const message = await reader();
+
+      if (message === null) {
+        break;
+      }
+
       debug('MSG:', message);
 
       const { data: data, error: error } = message;
@@ -196,7 +187,8 @@ export async function fetchAndStream(port, messages) {
     if (error === PORT_CLOSED) {
       return;
     } else {
-      console.error(error);
+      console.trace('ERROR', error);
+      //console.error(error);
     }
   }
 }
